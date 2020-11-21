540bd69fe615a779708579df1b354ce7baa80009
saiatrija74 <saiteja3074@gmail.com>
2020-11-21 21:55:02 +0530

    commmit changes after git stash

--- FirstFile.java
+++ FirstFile.java
@@ -3,5 +3,7 @@ public class FirstFile{
 	public static void main(){
 		System.out.println("");
 	}	
-
+	public static void afterStash(){
+		System.out.println("after git stash done applying changes");
+	}
 }
